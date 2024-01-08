934c21f6df2a72d5b229d238fdcc9d68775f0127
zachery chin <zachery@getklarity.ai>
2024-01-08 12:32:04 -0800

    Finished creating test world state from json
    Also creates the relationships as defined in the "relationships" object
    in the input json.
    
    This means we have all the basic functionality needed to create a graph
    structure out of the entities and relationships extracted by the LLM
    
    need to create a better means of defining the relationship type,
    currently need to write a separate query for each relationship type.
    Also need to test what happens if attempt to create a node which already
    exists. This should work due to using MERGE but this is untested. It
    should also not overwrite the existing node but do something smart with
    description e.g. append further description on top of what exists

--- dungeon_master/src/dungeon_master/fixtures/test_world_state.clj
+++ dungeon_master/src/dungeon_master/fixtures/test_world_state.clj
@@ -2,7 +2,6 @@
   (:import [org.neo4j.driver GraphDatabase]
            [org.neo4j.driver AuthTokens]
            [org.neo4j.driver Values]
-           ;;[org.neo4j.driver Values parameters]
            [org.neo4j.driver TransactionWork])
   (:require [cheshire.core :as json]))
 
@@ -45,22 +44,25 @@
   (with-open [driver (GraphDatabase/driver "bolt://graphdb:7687" (AuthTokens/none))]
   ;;(with-open [driver (GraphDatabase/driver "bolt://localhost:7687" (AuthTokens/none))]
       (with-open [session (.session driver)]
-        (let [world-data (json/parse-string fixture-json-string)]
-          ( let [entities (world-data "entities")]
+        (let [world-data (json/parse-string fixture-json-string)
+              entities (world-data "entities")
+              relationships (world-data "relationships") ]
 
-            (doall (map
-              (fn [entity-data] (create-node entity-data session))
-              entities)))))))
+          (doall (map
+                   (fn [entity-data]
+                     (create-node entity-data session))
+                   entities))
 
-(insert-test-world-state)
+          (doall (map
+                   (fn [relationship-data]
+                     (create-relationship-from-string relationship-data session))
+                   relationships))))))
 
 (defn create-node
   [node-data driver-session]
   (case (node-data "label")
     "Place" (create-place-node node-data driver-session)
-    "Person" (create-person-node node-data driver-session)
-    )
-  )
+    "Person" (create-person-node node-data driver-session)))
 
 (defn create-person-node
   "create a person node"
@@ -74,12 +76,41 @@
   [node-data driver-session]
 
   ;;(println "DEBUG: called create-place-node")
-  (let [cypher-string "MERGE (p:Place {id: $id, name: $name, description: $description}) RETURN (p)"]
+  (let [cypher-string "MERGE (p:Place {id: $id, name: $name, description: $description}) RETURN (p)" ]
+        ;;parameters (Values/parameters
+        ;;             ;;(into {} (map (fn [[k v]] [(name k) v]) node-data))
+        ;;             "id" ("id" node-data)
+        ;;             "name" ("name" node-data)
+        ;;             "description" ("description" node-data)) ]
     (run-cypher-stmt-with-data cypher-string node-data driver-session)))
+
+(defn create-relationship-from-string
+  "relate two nodes given the input string of the format
+   node_1_id|RELATIONSHIP_TYPE|node_2_id"
+  [input driver-session]
+  (let [[cypher-query cypher-params] (apply create-relationship-statement (decompose-relationship-string input))]
+    (run-cypher-stmt-with-data-no-return cypher-query cypher-params driver-session)))
+
+(defn create-relationship-statement
+  "relate two nodes with each other"
+  [first-node-id relationship-type second-node-id]
+  (let [cypher-stmt (case relationship-type
+                      "IN" "MATCH (n1) WHERE n1.id = $start_node_id
+                           MATCH (n2) WHERE n2.id = $end_node_id
+                           CREATE (n1)-[:IN]->(n2)")
+        cypher-params {"start_node_id" first-node-id "end_node_id" second-node-id} ]
+    [cypher-stmt cypher-params]))
+
+(defn decompose-relationship-string
+  "convert string of form
+  node_1_id|RELATIONSHIP_TYPE|node_2_id
+  to a seq of (node-1 relationship-type node-2) each of which are strings"
+  [input]
+  (clojure.string/split input #"\|"))
 
 
 (defn run-cypher-stmt-with-data
-  "Run a MERGE statement along with data to fill cypher placeholders"
+  "Run a cypher statement along with data to fill cypher placeholders"
   [cypher-statement node-data driver-session]
 
   (.writeTransaction
@@ -89,21 +120,45 @@
                                    (.run tx
                                          cypher-statement
                                          node-data)]
-                               (.single result)
-                               )))))
+                               (.single result))))))
 
+(defn run-cypher-stmt-with-data-no-return
+  "Run a cypher statement along with data to fill cypher placeholders"
+  [cypher-statement node-data driver-session]
+
+  (.writeTransaction
+    driver-session
+    (reify TransactionWork (execute [this tx]
+                             (let [result
+                                   (.run tx
+                                         cypher-statement
+                                         node-data)])))))
 ;; next steps
-;; TODO: create the relationships as defined in the input json
-
+;; TODO: use Values/parameters rather than just clojure maps to pass the args
+  ;; couldn't get this to work. complains of 6 params being passed to Values.parameters
+  ;; other means of calling `parameters` seemed to complain of things relating
+  ;; to clojure data types not matching up with java datatypes
 
 
 ;; testing this out
+
 
 ;; Run these in repl or editor to have access to the imports needed in this namespace
 ;;(import '(org.neo4j.driver TransactionWork))
 ;;(import '(org.neo4j.driver GraphDatabase))
 ;;(import '(org.neo4j.driver AuthTokens))
+;;(import '(org.neo4j.driver Values))
 ;;(require '[cheshire.core :as json])
+
+
+;;(insert-test-world-state)
+
+;;(defn test-create-relationship
+;;  "insert some fixed data into the graph db"
+;;  []
+;;  (with-open [driver (GraphDatabase/driver "bolt://localhost:7687" (AuthTokens/none))]
+;;      (with-open [session (.session driver)]
+;;        (create-relationship-from-string  "coran|IN|blushingMermaidTavern" session))))
 
 ;; From within the compose cluster, use graphdb as the hostname of the neo4j instance
 ;; but running in my repl, i can access it as localhost
