beea379bfaccb5e8e679fea379a93f2b53c6d8d9
Zachery Chin <zchin@sproutpost.net>
2024-02-23 18:58:53 -0800

    add some commands that can be entered
    /quit to quit the game
    /debug for later implementation, but for now it proves that issuing a
    command doesn't interact with the game itself, and instead interacts
    with the engine

--- dungeon_master/src/dungeon_master/interface/cli.clj
+++ dungeon_master/src/dungeon_master/interface/cli.clj
@@ -5,7 +5,8 @@
             ))
 
 
-(declare mock-run-turn)
+
+(declare parse-user-command)
 ;; I'm not sure how this should work so this is just scratch until i get a
 ;; better handle on it
 (defn game-loop
@@ -23,5 +24,57 @@
     (let [user-command (read-line)]
       ;;(println "DEBUG: spitting back input: " user-command)
       ;;(println "DEBUG: last 3 interactions: " (take-last 3 (:interaction-history curr-state)))
-      (recur (run-turn curr-state user-command)))
-    ))
+      (recur
+        (if-not (parse-user-command user-command) (run-turn curr-state user-command))))))
+
+(defn quit-game []
+  ;; save the game or something
+  (println "Goodbye!")
+  (System/exit 0)
+  true)
+
+(defn test-command []
+  (println "TEST COMMAND RECEIVED")
+  true)
+
+(defn parse-user-command
+  "handle non-game command input. Things like exit game or print debug info"
+  [user-command]
+  (let [command-list [[#"^/exit" quit-game]
+                       [#"^/quit" quit-game]
+                       [#"^/debug" test-command]]
+        matched-command (some
+                          (fn [[command-regex action-function]]
+                             (when (re-find command-regex user-command) action-function))
+                          command-list)]
+
+    (if matched-command (matched-command) user-command)))
+
+;;(parse-user-command "hobos")
+
+;;(defn parse-command
+;;  [input]
+;;  (let [command-list '([#"^/exit" quit-game]
+;;                       [#"^/quit" quit-game]
+;;                       [#"^/debug" test-command])
+;;        matched-command (some
+;;                          (fn [[command-regex action-function]]
+;;                             (when (re-find command-regex user-command) action-function))
+;;                          command-list)]
+;;
+;;    (println (str "DEBUG: by here, what is matched-command?" matched-command))
+;;    (if matched-command (matched-command) nil)))
+
+
+;;(def command-list '([#"/exit" "exit command match"]
+;;                   [#"/quit" "quit command match"]
+;;                   [#"/debug" "debug command match"]))
+;;
+;;(defn test-command-matcher
+;;  [input]
+;;  (some
+;;    (fn [[regex output]] (when (re-find regex input) output))
+;;    command-list)
+;;  )
+;;(test-command-matcher "/quit")
+
